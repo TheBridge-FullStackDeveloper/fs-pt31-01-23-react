08ac7b58256a1ca6ed0a4c421cdd302fa79a004f
OmarjLand <omar.land2010@gmail.com>
2023-05-22 19:57:29 +0200

    Añado el HeadCards

--- react-tb-landing-page/src/components/App.jsx
+++ react-tb-landing-page/src/components/App.jsx
@@ -1,19 +1,21 @@
 import "../App.css";
 import Navbar from "./Navbar/Navbar"; // Importo el component Navbar
 import Container from "./Container/Container"; // Importo el component Container
+import HeadCards from "./HeadCards/HeadCards"; // Importo el component HeadCards
 
 // Links lo uso para meter el menú de mi Component -> NavBar
 const links = ["Cursos", "Dónde estudiar", "Financiación y becas", "Empresas e instituciones","Sobre nosotros"]
-
-
 
 function App() {
   return (
   
     // <div>Hello React Primera Vista</div>
     <div>
+      
       <Navbar links={links}/>
       <Container />
+      <HeadCards />
+
     </div>
   
   );
--- react-tb-landing-page/src/components/Container/Container.jsx
+++ react-tb-landing-page/src/components/Container/Container.jsx
@@ -5,25 +5,30 @@ function Container(){
 
     return(<>
 
-      <div className="container">
+      <div className="container back-opacity">
         <section className="cont-left">
             
-            <h1 className="square-element acelera">Acelera tu carrera digital</h1>
-            <br />
-            
+            <h1 className="square-element acelera">Acelera tu carrera digital</h1>            
             <p className="text1"><strong>En una época de cambios y desarrollo tecnológico, tu formación no puede perder el ritmo.</strong></p>
-
             <p className="text2">Cuando eliges un bootcamp en The Bridge estás dando el paso definitivo hacia una de las profesiones del futuro más demandadas en el presente, ya sea Ciberseguridad, Data Science, Desarrollo de Software, Diseño de Producto UX/UI o Marketing Digital</p>
+
+            <button className="pideInfo">
+                <strong>PIDE INFORMACIÓN</strong>
+            </button>
+
+            
 
         </section>
         
         <section className="cont-right">
             <img src="././src/assets/imgs/students_copy.png"/>
         </section>
+
+
       </div>
 
 
-  
+      <div className="divisor"></div>
     </>)
   
   }
