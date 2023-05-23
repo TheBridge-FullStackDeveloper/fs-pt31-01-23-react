55bc05ddf1cebc100834c5cca5781932a488632c
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-05-23 21:26:15 +0200

    html y CSS body

--- react-tb-landing-page/src/body.jsx
+++ react-tb-landing-page/src/body.jsx
@@ -4,20 +4,20 @@ function Body(){
         <div className="text">
         <h1>
             Acelera tu carrera digital
-        </h1><br/>
+        </h1>
         <h2> 
         En época de cambios y desarrollo tecnológico, tu formación no puede perder el ritmo.
-        </h2><br/>
+        </h2>
         <p>
         Cuando eliges un bootcamp en The Bridge estás dando el paso definitivo hacia las profesiones del futuro más demandadas en el presente, ya sea Ciberseguridad, Data Science, Desarrollo Web Full Stack, UX/UI Product Design, Marketing Digital o Cloud & DevOps.
-        </p><br/>
-        <button>
+        </p>
+        <button className="buttonBody">
             PIDE INFORMACIÓN 
-        </button><br/>
+        </button>
         </div>
 
         <div className="imgBody">
-            <img src = "https://github.com/TheBridge-FullStackDeveloper/thebridge-homepage-styledcomponent-exercise/blob/main/imgs/students.png?raw=true"></img>
+            <img src = "https://uploads-ssl.webflow.com/60780bff57ddc42a6adc1d7e/60e4408a7bd856103b6000f5_DSC03804%20(2).jpg"></img>
         </div>
     </div>
 
