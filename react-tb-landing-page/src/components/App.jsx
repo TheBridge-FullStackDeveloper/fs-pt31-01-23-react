bb2a9b807f2002ad4c7ff2342927ee530da1fb0b
OmarjLand <omar.land2010@gmail.com>
2023-05-23 21:48:36 +0200

    Añado el componente de Card

--- react-tb-landing-page/src/components/App.jsx
+++ react-tb-landing-page/src/components/App.jsx
@@ -2,6 +2,7 @@ import "../App.css";
 import Navbar from "./Navbar/Navbar"; // Importo el component Navbar
 import Container from "./Container/Container"; // Importo el component Container
 import HeadCards from "./HeadCards/HeadCards"; // Importo el component HeadCards
+import CardsContainers from "./CardsContainers/CardsContainers"; // Importo el component CardsContainers
 
 // Links lo uso para meter el menú de mi Component -> NavBar
 const links = ["Cursos", "Dónde estudiar", "Financiación y becas", "Empresas e instituciones","Sobre nosotros"]
@@ -15,6 +16,21 @@ function App() {
       <Navbar links={links}/>
       <Container />
       <HeadCards />
+      <br />
+
+      <section className="flex-cards">
+
+        <CardsContainers 
+          title="Ciberseguridad" 
+          img="https://uploads-ssl.webflow.com/60782404b8e195845053206a/60d0a6ee92195f77a8ac4bfe_CS.svg" 
+          time="480" 
+          modo="Presencial/Remoto" 
+          description="Con nuestro bootcamp adquirirás los conocimientos para ser miembro de un equipo Red Team o Blue Team utilizando las estrategias y herramientas más actuales, en uno de los mejores programas del mundo en 2021" 
+          info="INFO" 
+        />
+
+      </section>
+      
 
     </div>
   
