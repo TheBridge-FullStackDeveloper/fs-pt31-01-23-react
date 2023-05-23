53134afc918a94aaf0019669d5f94c4ca7aac178
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-05-23 21:29:23 +0200

    html bootcamps

---
+++ react-tb-landing-page/src/bootcamps.jsx
@@ -0,0 +1,35 @@
+function Bootcamp (){
+    return (
+    <div>
+        <h1 className="h1Bootcamp">Descubre nuestros bootcamp</h1>
+        <div className = "ciberseguridad">
+            <h2>Ciberseguridad</h2>
+
+        </div>;
+
+        <div className="datascience">
+            <h2>Data Science</h2>
+
+        </div>;
+
+        <div className="fullStack">
+            <h2>DesarrolloWeb Full Stack</h2>
+
+        </div>;
+        
+        <div class="marketing">
+            <h2>Digital Marketing OPS</h2>
+
+        </div>;
+
+        <div className="desing">
+            <h2>UX/UI Product Desing</h2>
+
+        </div>
+    </div>
+        
+    )
+ 
+  }
+
+  export default Bootcamp;
