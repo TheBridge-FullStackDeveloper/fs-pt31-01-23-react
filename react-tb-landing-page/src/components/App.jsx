f7336f1961b539229a74b0dea5552ca0d5e53854
OmarjLand <omar.land2010@gmail.com>
2023-05-24 00:44:24 +0200

    Añado las tarjetas de Bts

--- react-tb-landing-page/src/components/App.jsx
+++ react-tb-landing-page/src/components/App.jsx
@@ -9,10 +9,10 @@ const links = ["Cursos", "Dónde estudiar", "Financiación y becas", "Empresas e
 
 function App() {
   return (
-  
+
     // <div>Hello React Primera Vista</div>
     <div>
-      
+
       <Navbar links={links}/>
       <Container />
       <HeadCards />
@@ -20,20 +20,57 @@ function App() {
 
       <section className="flex-cards">
 
-        <CardsContainers 
-          title="Ciberseguridad" 
-          img="https://uploads-ssl.webflow.com/60782404b8e195845053206a/60d0a6ee92195f77a8ac4bfe_CS.svg" 
-          time="480" 
-          modo="Presencial/Remoto" 
-          description="Con nuestro bootcamp adquirirás los conocimientos para ser miembro de un equipo Red Team o Blue Team utilizando las estrategias y herramientas más actuales, en uno de los mejores programas del mundo en 2021" 
-          info="INFO" 
+        <CardsContainers
+          title="Ciberseguridad"
+          img="https://uploads-ssl.webflow.com/60782404b8e195845053206a/60d0a6ee92195f77a8ac4bfe_CS.svg"
+          time="480"
+          modo="Presencial/Remoto"
+          description="Con nuestro bootcamp adquirirás los conocimientos para ser miembro de un equipo Red Team o Blue Team utilizando las estrategias y herramientas más actuales, en uno de los mejores programas del mundo en 2021"
+          info="INFO"
         />
 
+        <CardsContainers
+          title="Data Science"
+          img="https://uploads-ssl.webflow.com/60782404b8e195845053206a/61b356a24ba5746ddec2dd6b_iconowebDS.png"
+          time="480"
+          modo="Presencial/Remoto"
+          description="Con nuestro bootcamp en Data Science aprenderás a analizar grandes volúmenes de datos combinando programación y matemáticas para resolver problemas complejos y obtener así información de valor para la toma de decisiones."
+          info="INFO"
+        />
+       
+        <CardsContainers
+          title="Desarrollo Full Stack"
+          img="https://uploads-ssl.webflow.com/60782404b8e195845053206a/60a64a28aaa7f58060aa5dcb_FS.svg"
+          time="480"
+          modo="Presencial/Remoto"
+          description="Con nuestro bootcamp en Desarrollo Web Full Stack adquirirás los conocimientos necesarios para desarrollar la estructura al completo de una web o web app."
+          info="INFO"
+        />
+
+        <CardsContainers
+          title="Marketing Digital"
+          img="https://uploads-ssl.webflow.com/60782404b8e195845053206a/60d0a6af5f8903204be404b0_DM.svg"
+          time="480"
+          modo="Presencial/Remoto"
+          description="Con nuestro bootcamp en Marketing Digital adquirirás los conocimientos para potenciar un negocio o marca de manera eficiente y así conseguir aportar valor a las empresas mejorando sus resultados de ventas y rentabilidad."
+          info="INFO"
+        />
+
+        <CardsContainers
+          title="UX/UI Product Design"
+          img="https://uploads-ssl.webflow.com/60782404b8e195845053206a/60a649f7b1cdbc9d2f9b93be_PD.png"
+          time="480"
+          modo="Presencial/Remoto"
+          description="Con nuestro bootcamp en UX/UI Product Design adquirirás los conceptos clave sobre la experiencia de usuario. Realiza investigaciones en profundidad sobre clientes potenciales, y diseña productos digitales que resuelvan sus necesidades actuales."
+          info="INFO"
+        />
+
+
       </section>
-      
+
 
     </div>
-  
+
   );
 }
 
--- react-tb-landing-page/src/components/CardsContainers/CardsContainers.jsx
+++ react-tb-landing-page/src/components/CardsContainers/CardsContainers.jsx
@@ -8,35 +8,32 @@ const CardsContainers = ( {title, img, time, modo, description, info} ) => {
         <div className="containBoxes">
             <section className="Cards">
                 <section className="flex-cont-head">
-
                     <h2 className="title">{title}</h2>
                     <img src={img} className="logo-bt"/>
-
                 </section>
 
                 <section className="flex-cont-card">
-
                     <p className="bg-black-time flex-cont-card"> 
                         <img src="https://uploads-ssl.webflow.com/60780bff57ddc42a6adc1d7e/60ab63e4d647d89630b5205c_Time%20Circle.svg" className="size-svg"/>
                         <div className="centering-text"> {time} </div>
                     </p>
 
-
                     <p className="bg-black-modo flex-cont-card">
                         <img src="https://uploads-ssl.webflow.com/60780bff57ddc42a6adc1d7e/60ab63bdeec8ef0ff6d3600d_Location.svg" className="size-svg"/>
                         <div className="centering-text"> {modo} </div>
                     </p>
-
                 </section>
 
-                <p className="description">{description}</p>
+                <section className="description">{description}</section>
+        
+                <div className="line-extra"></div>
 
                 <div className="info">+ {info}</div>  
-            </section>
+
+            </section>        
         </div>
 
     </>)
-
 }
 
 
