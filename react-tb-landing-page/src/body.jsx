00401c9204eddd8620e1b459f6156bba85364f93
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-05-23 19:30:59 +0200

    html body

---
+++ react-tb-landing-page/src/body.jsx
@@ -0,0 +1,27 @@
+function Body(){
+    return (<>
+    <div className="body">
+        <div className="text">
+        <h1>
+            Acelera tu carrera digital
+        </h1><br/>
+        <h2> 
+        En época de cambios y desarrollo tecnológico, tu formación no puede perder el ritmo.
+        </h2><br/>
+        <p>
+        Cuando eliges un bootcamp en The Bridge estás dando el paso definitivo hacia las profesiones del futuro más demandadas en el presente, ya sea Ciberseguridad, Data Science, Desarrollo Web Full Stack, UX/UI Product Design, Marketing Digital o Cloud & DevOps.
+        </p><br/>
+        <button>
+            PIDE INFORMACIÓN 
+        </button><br/>
+        </div>
+
+        <div className="imgBody">
+            <img src = "https://github.com/TheBridge-FullStackDeveloper/thebridge-homepage-styledcomponent-exercise/blob/main/imgs/students.png?raw=true"></img>
+        </div>
+    </div>
+
+            </>)
+  }
+
+  export default Body;
