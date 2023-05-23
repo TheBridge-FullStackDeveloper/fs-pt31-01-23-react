62b9cf1b687669f71578bfe210dad148e2fe4f16
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-05-23 18:23:50 +0200

    html y CSS Navbar

---
+++ react-tb-landing-page/src/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar(){
+    return (
+    <div className="navbar2">
+      <img className = "navar2Img" src="https://uploads-ssl.webflow.com/60780bff57ddc42a6adc1d7e/607eeb4b0517b6659206c10f_thebridgelogo.svg"/>
+  
+      <ul>
+      <li> Cursos &#9660;</li>
+      <li>Dónde estudiar &#9660;</li>
+      <li>Financiación y becas &#9660;</li>
+      <li>Empresas e instituciones &#9660;</li>
+      <li>Sobre nosotros &#9660;</li>
+    </ul>
+    </div>)
+  }
+
+  export default Navbar;
