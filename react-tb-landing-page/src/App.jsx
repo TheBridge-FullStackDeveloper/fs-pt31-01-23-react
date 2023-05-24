97a6da4566c8e94a1dda221663b90ba49c0da74d
Nuria Ruiz Molina <nuriaruizmolina@MacBook-Pro-de-Nuria.local>
2023-05-24 22:53:38 +0200

    Faltan estilos de Bootcamps

--- react-tb-landing-page/src/App.jsx
+++ react-tb-landing-page/src/App.jsx
@@ -1,7 +1,21 @@
+import React from "react";
 import "./App.css";
+import Navbar from "../components/Navbar/Navbar";
+import Background from "../components/Background/Background";
+import Bootcamps from "../components/Bootcamps/Bootcamps";
+
+
+const links = ['Cursos','Donde Estudiar','Financiacion y Becas','Empresas e Instituciones','Sobre nosotros']
+
 
 function App() {
-  return <div>Hello React</div>;
+  
+  return  <div>
+    <Navbar links={links}/>
+    <Background/>
+    <Bootcamps/>
+  </div>;
 }
+
 
 export default App;
