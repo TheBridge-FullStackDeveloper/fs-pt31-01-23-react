0ee80d1d67843ab7355ab06a357a7a3e6de81c28
ChanaFerrari514 <charlyneferrari@gmail.com>
2023-05-24 19:45:05 +0200

    ...

--- react-tb-landing-page/react-tb-landing-page/src/components/Header.jsx
+++ react-tb-landing-page/react-tb-landing-page/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import "./Header.css";
 
 const headerData = {
-    title: "Acelera tu carrera digital",
+    title: "Acelera tu carrera digital.",
     subheading: "En una época de cambios y desarrollo tecnológico, tu formación no puede perder el ritmo.",
     paragraph: "Cuando eliges un bootcamp en The Bridge estás dando el paso definitivo hacia una de las profesiones del futuro más demandadas en el presente, ya sea Ciberseguridad, Data Science, Desarrollo de Software, Diseño de Producto UX/UI o Marketing Digital.",
     button: "pide información"
@@ -10,18 +10,7 @@ const headerData = {
 function Header() {
     return(
         <header className="header">
-            <h1 className="title">
-                {headerData.title.split('.').map((part, index) => (
-                    index === 0 ? (
-                        <span key={index}>
-                            {part}.
-                        </span>
-                    ) : (
-                        part
-                    )
-                    ))}
-                    </h1>
-
+            <h1 className="title">{headerData.title}</h1>
             <h2 className="subheading">{headerData.subheading}</h2>
             <p className="paragraph">{headerData.paragraph}</p>
             <button className="info-button">{headerData.button}</button>
