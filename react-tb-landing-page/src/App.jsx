61bc956a820af6c541e3905a323bb72ec0a313ee
Ivan Romero <ivanromero5520@gmail.com>
2023-06-01 20:07:10 +0200

    aa

--- react-tb-landing-page/src/App.jsx
+++ react-tb-landing-page/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 //import "./components/Navbar/Navbar.jsx"
 //import Bootcamp from "./components/Bootcamp/Bootcamps.jsx";
-import "../data.json";
+import data from "../data.json"
 
 
 function App() {
@@ -11,44 +11,8 @@ const links = ["Cursos", "Donde estudiar", "Financiacion y becas",
  
                          //cartas de los bootcamps
 
- const bootcamps = {
-    "cs": {
-      "title": "Ciberseguridad",
-      "img": "https://uploads-ssl.webflow.com/60782404b8e195845053206a/60d0a6ee92195f77a8ac4bfe_CS.svg",
-      "hours": "480",
-      "format": "Presencial/Remoto",
-      "description": "Con nuestro bootcamp adquirirás los conocimientos para ser miembro de un equipo Red Team o Blue Team utilizando las estrategias y herramientas más actuales, en uno de los mejores programas del mundo en 2021."
-    },
-    "ds": {
-      "title": "Data Science",
-      "img": "https://uploads-ssl.webflow.com/60782404b8e195845053206a/60a649ad574f4fc2cffa3f94_DS.svg",
-      "hours": "480",
-      "format": "Presencial/Remoto",
-      "description": "Aprenderás Python, sus librerías de análisis de datos y de Machine Learning y Deep para construir y desplegar modelos y mostrar su utilidad al negocio."
-    },
-    "fs": {
-      "title": "Desarrollo Web Full Stack",
-      "img": "https://uploads-ssl.webflow.com/60782404b8e195845053206a/60a64a28aaa7f58060aa5dcb_FS.svg",
-      "hours": "480",
-      "format": "Presencial/Remoto",
-      "description": "Con nuestro bootcamp te transformarás en un desarrollador Web Full Stack. Aprende desde cero HTML, JavaScript node.JS y bases de datos. Acelera tus proyectos utilizando ReactJS, y conoce los fundamentos de DevOps para el despliegue y mantenimiento de tus proyectos."
-    },
-    "dm": {
-      "title": "Digital Marketing OPS",
-      "img": "https://uploads-ssl.webflow.com/60782404b8e195845053206a/60d0a6af5f8903204be404b0_DM.svg",
-      "hours": "480",
-      "format": "Presencial/Remoto",
-      "description": "Una aproximación práctica al Marketing Digital que te permitirá conocer las ultimas herramientas y los nuevos medios, con foco en la planificación, análisis y automatización para la optimización de resultados. * Becas L'Oréal Digital Marketing Ops"
-    },
-    "ux": {
-      "title": "UX/UI Product Design",
-      "img": "https://uploads-ssl.webflow.com/60782404b8e195845053206a/60a649f7b1cdbc9d2f9b93be_PD.png",
-      "hours": "480",
-      "format": "Presencial/Remoto",
-      "description": "Te familiarizarás con conceptos clave sobre experiencia de usuario: metodologías de investigación y análisis de datos, arquitectura de la información, ideación y principios del diseño visual y de experiencia de usuario. * Becas Opinno Digital Experience"
-    }
-  }
-
+  const cursos = [data.bootcamps.cs, data.bootcamps.dm, data.bootcamps.ds, data.bootcamps.fs, data.bootcamps.ux]
+  console.log(cursos)
   return (
 <>
 <div className="backimage">
@@ -74,13 +38,34 @@ const links = ["Cursos", "Donde estudiar", "Financiacion y becas",
     <div className="red"></div> 
     <h1 className="h1">Descubre nuestros Bootcamps</h1>
  </div>
- <div className="cartasBootcamp">
-  
- 
-  
+ <div className="cartas">
+
+ <img className="card-image"
+      src={cursos.img}
+    ></img>
+    <h3 className="h3-card">{cursos.title}</h3>
+    <button className="button-card" >{cursos.hours}</button>
+    <button className="location-card">{cursos.format}</button>
+    <p className="p-card">{cursos.description}</p>
+    <p className="info">+ INFO</p>
+{
+    cursos.map((cursos, idx) =>
+    <div key={idx} className="containers-total">
+       cursos={cursos}
+       </div>
+       )
+}
+
+
+
 
  </div>
-// no consigo importar ni definir los bootcamps
+ 
+ 
+
+
+
+   
 
 
 
