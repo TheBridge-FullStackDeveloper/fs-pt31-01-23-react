482f60a3440313db0d44dff90e087eb56bc41997
OmarjLand <omar.land2010@gmail.com>
2023-06-07 00:32:20 +0200

    Preparo mi enviroment

---
+++ exercise-advanced-pokedex/src/components/App.jsx
@@ -0,0 +1,26 @@
+import "../App.css";
+
+function NavBar(){
+  return(
+    <>
+      <ul>
+        <li>Pokedex</li>
+        <li>CualquierCosa</li>
+        <li>Volver</li>
+      </ul>
+    </>
+  )
+}
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar />  
+     <h1><i>Hello React PORFIN ME VAAA!!!!!</i></h1>
+    </>
+  )
+}
+
+export default App
